6780fb1268508da518433a5c13d9d2be20dfe262
zhongkaiz <985691039@qq.com>
2023-01-15 18:48:06 +0800

    fix ""

--- src/avlog.c
+++ src/avlog.c
@@ -1,9 +1,10 @@
 #include "libavutil/log.h"
 
 int main(int argc, char ** argv)
+//gcc avlog.c -I../include/ -L../lib/ -lavutil
 {
 	av_log_set_level(AV_LOG_DEBUG);
-	av_log(NULL, AV_LOG_ERROR, 'this is error log level!\n');
+	av_log(NULL, AV_LOG_ERROR, "this is error log level!\n");
 	return 0;
 }
 
