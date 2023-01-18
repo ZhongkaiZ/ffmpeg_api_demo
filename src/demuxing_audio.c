09d20673ecb2b97c19973b3bd49398b36c80fdd7
ZhongkaiZ <985691039@qq.com>
2023-01-18 15:38:44 +0800

    Update demuxing_audio.c
    add ADTS header

--- src/demuxing_audio.c
+++ src/demuxing_audio.c
@@ -1,6 +1,62 @@
 #include "libavutil/log.h"
 #include "libavformat/avformat.h"
 
+const int sampleFrequencyTable[] = {
+	96000,
+	88200,
+	64000,
+	48000,
+	44100,
+	32000,
+	24000,
+	22050,
+	16000,
+	12000,
+	11025,
+	8000,
+	7350
+};
+
+int getADTSHeader(char *adtsHeader, int packetSize, int profile, int sampleRate, int channels)
+{
+	int sampleFrequenceIndex = 3;	
+	int adtsLength = packetSize + 7;
+	
+	for(int i = 0; i < sizeof(sampleFrequencyTable) / sizeof(sampleFrequencyTable[0]); i++)
+	{
+		if(sampleRate == sampleFrequencyTable[i])
+		{
+			sampleFrequenceIndex = i;
+			break;
+		}
+	}
+	adtsHeader[0] = 0xff;		//syncword:0xfff                          8bits
+
+	adtsHeader[1] = 0xf0;		//syncword:0xfff                          4bits
+	adtsHeader[1] |= (0 << 3);	//MPEG Version:0 for MPEG-4,1 for MPEG-2  1bit
+	adtsHeader[1] |= (0 << 1);	//Layer:0                                 2bits
+	adtsHeader[1] |= 1;			//protection absent:1                     1bit
+
+	adtsHeader[2] = (profile << 6);							//profile:profile               2bits
+	adtsHeader[2] |= (sampleFrequenceIndex & 0x0f) << 2;	//sampling frequency index:sampling_frequency_index  4bits
+	adtsHeader[2] |= (0 << 1);								//private bit: 0                   1bit
+	adtsHeader[2] |= (channels & 0x04) >> 2;				//channel configuration:channels  1bit
+
+	adtsHeader[3] = (channels & 0x03) << 6;					//channel configuration:channels 2bits
+	adtsHeader[3] |= (0 << 5);								//original:0				1bit
+	adtsHeader[3] |= (0 << 4);								//home:0                	1bit
+	adtsHeader[3] |= (0 << 3);								//copyright id bit:0       	1bit
+	adtsHeader[3] |= (0 << 2);								//copyright id start:0		1bit
+	adtsHeader[3] |= ((adtsLength & 0x1800) >> 11);			//frame length:value   		2bits
+
+	adtsHeader[4] = (uint8_t)((adtsLength & 0x7f8) >> 3);	//frame length:value    8bits
+	adtsHeader[5] = (uint8_t)((adtsLength & 0x7) << 5);		//frame length:value    3bits
+	adtsHeader[5] |= 0x1f;									//buffer fullness:0x7ff 5bits
+	adtsHeader[6] = 0xfc;									//buffer fullness:0x7ff 6bits
+	return 0;
+}
+
+
 int main(int argc, char**argv)
 {
     //gcc demuxing_audio.c -I../include/ -L../lib -lavutil -lavformat -lavcodec
@@ -49,6 +105,15 @@ int main(int argc, char**argv)
 	}
 	while(av_read_frame(inputFoarmatCtx, &packet)==0){
 		if(packet.stream_index == audioIndex){
+			char adtsHeader[7] = {0};
+			getADTSHeader(adtsHeader, packet.size, inFormatCtx->streams[audioIndex]->codecpar->profile,   //写入adts头
+				inFormatCtx->streams[audioIndex]->codecpar->sample_rate, 
+				inFormatCtx->streams[audioIndex]->codecpar->channels);
+			ret = fwrite(adtsHeader, 1, sizeof(adtsHeader), dest_fp);
+			if(ret != sizeof(adtsHeader))
+			{
+				//
+			}
 			ret = fwrite(packet.data, 1, packet.size, dest_fp);
 			if (ret != packet.size){
 				av_log(NULL,AV_LOG_ERROR,"write file failed!\n");
