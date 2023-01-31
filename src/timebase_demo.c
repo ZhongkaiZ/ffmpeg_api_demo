804d3eeb57d0c2152ee354dd8769e7c566f4fb02
ZhongkaiZ <985691039@qq.com>
2023-01-31 14:39:30 +0800

    add function
    add

--- src/timebase_demo.c
+++ src/timebase_demo.c
@@ -26,6 +26,21 @@ int main(int argc, char ** argv)
 
 	av_log(NULL, AV_LOG_INFO, "input file duration:%ld ms,duration time %lf s.\n",inFmtCtx->duration, inFmtCtx->duration * av_q2d(AV_TIME_BASE_Q));
 
+
+	AVRational videoTimeBase;
+	AVRational audioTimeBase;
+
+	for(int i=0; i< inFmtCtx->nb_streams; i++){
+		AVStream *inStream = inFmtCtx->streams[i];
+		if(inStream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO){
+			videoTimeBase = inStream->time_base;
+			av_log(NULL,AV_LOG_INFO,"video time base: num = %d, den = %d\n",videoTimeBase.num,videoTimeBase.den);
+		}
+		if(inStream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO){
+			audioTimeBase = inStream->time_base;
+			av_log(NULL,AV_LOG_INFO,"audio time base: num = %d, den = %d\n",audioTimeBase.num,audioTimeBase.den);
+		}
+	}
 	
 	return 0;
 }
