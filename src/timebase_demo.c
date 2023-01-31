6d3c7a83b2ce0a370632779c816e821b42d5d17c
ZhongkaiZ <985691039@qq.com>
2023-01-31 15:15:35 +0800

    fix a bug
    fix a bug

--- src/timebase_demo.c
+++ src/timebase_demo.c
@@ -48,7 +48,7 @@ int main(int argc, char ** argv)
 
 	while(av_read_frame(inFmtCtx, &packet)==0){
 		AVStream * inStream = inFmtCtx->streams[packet.stream_index];
-		av_log(NULL,AV_LOG_INFO,"streamIndex = %d, pts=%d, pts_time = %lf dts=%d, dts_time = %lf\n",\
+		av_log(NULL,AV_LOG_INFO,"streamIndex = %d, pts=%ld, pts_time = %lf dts=%ld, dts_time = %lf\n",\
 			packet.stream_index,\
 			packet.pts, packet.pts*av_q2d(inStream->time_base),\
 			packet.dts, packet.dts*av_q2d(inStream->time_base));
